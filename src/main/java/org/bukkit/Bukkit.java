94785ef2adbd99d30b1ded79fb3b81c3fee2a2dd
Hexeption <minecraftfun201@gmail.com>
2020-09-24 20:44:23 +0100

    Added Spigot Bukkit Skeleton Patch

--- src/main/java/org/bukkit/Bukkit.java
+++ src/main/java/org/bukkit/Bukkit.java
@@ -1574,4 +1574,9 @@ public final class Bukkit {
     public static UnsafeValues getUnsafe() {
         return server.getUnsafe();
     }
+
+    @NotNull
+    public static Server.Spigot spigot(){
+        return server.spigot();
+    }
 }
